e8345e5f567fc00b648d1d48eae56f4015c1f6ac
Ahmed <ahmedsalam.2008@gmail.com>
2014-10-25 18:51:02 -0400

    Blog: checkpoint #1
    - Can view blog post entries on home page
    - Can create new blog entries
    - PostGres DB now works in local env

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 var express = require('express')
 var app = express();
-var cool = require('cool-ascii-faces')
-var pg = require('pg')
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cool = require('cool-ascii-faces');
+var pg = require('pg');
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser.json()); // supports JSON-encoded bodies
+app.use(bodyParser.urlencoded({  // to support URL encoded bodies
+  extended: true
+}));
+app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+//app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+
+var articleProvider = new ArticleProvider();
+
+app.get('/', function(req, res) {
+  articleProvider.findAll(function(error, docs) {
+    res.render('index.jade', {
+      title: 'Blog',
+      articles:docs
+    });
+  });
+});
+
+app.get('/blog/new', function(req, res) {
+  res.render('blog_new.jade', {
+    title: 'New Post'
+  });
+});
+
+app.post('/blog/new', function(req, res) {
+  articleProvider.save({
+    title: req.body.title,
+    body: req.body.body
+    }, function (error, docs) {
+      res.redirect('/');
+  });
+});
+
+app.get('/sample', function(request, response) {
   var result = ''
   var times = process.env.TIMES || 5
   for (i=0; i < times; i++)
